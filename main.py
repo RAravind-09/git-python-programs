ecaffad845f48ef5d0430d9bebd6e10181f7ec05
Aravind <aravindrani2003@gmail.com>
2024-06-19 08:32:49 +0530

    Added sample torch program

--- main.py
+++ main.py
@@ -38,10 +38,10 @@ print("torch" , x_np)
     # unless explicitly overridden.
 
 
-x_ones = torch.ones_like(x_data) # retains the properties of x_data
+x_ones = torch.ones_like(x_data)
 print(f"Ones Tensor: \n {x_ones} \n")
 
-x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
+x_rand = torch.rand_like(x_data, dtype=torch.float)
 print(f"Random Tensor: \n {x_rand} \n")
 
     # With random or constant values:
